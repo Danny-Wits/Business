ecd059574b84fc002c529c5bf2b65de29f2bc16b
Danishwer _ <dannywits12321@gmail.com>
2024-08-28 00:05:00 +0530

    Added section 3 and scroll logic Co-authored-by: single00double@gmail.com <single00double@gmail.com>

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 let index = 0;
 let running = true;
+let mobile = window.innerWidth < 600;
 const imageItemList = document.getElementsByClassName("img-item");
 const pauseIcon = document.getElementById("pause-icon");
 
@@ -72,10 +73,12 @@ const startAnimation = () => {
 
 document.addEventListener("DOMContentLoaded", startAnimation);
 
+//nav toggle
 const navLinks = document.getElementById("link");
 
 let active = false;
 const toggle = () => {
+  if (!mobile) return;
   active = !active;
   navLinks.style.display = active ? "flex" : "none";
   document.getElementsByTagName("nav")[0].style.backdropFilter = active
@@ -83,10 +86,12 @@ const toggle = () => {
     : "blur(0px)";
 };
 
+//s2 hover
 const s2Links = document.getElementsByClassName("list-container");
+const s2LinksAnchors = document.getElementsByClassName("list-item");
 const s2ImgContainer = document.getElementById("img-div");
 let isMouseOver = false;
-console.log(Array.from(s2Links));
+
 Array.from(s2Links).forEach((element) => {
   element.addEventListener("mouseenter", () => {
     element.classList.remove("none");
@@ -101,6 +106,7 @@ Array.from(s2Links).forEach((element) => {
   });
 });
 
+//s2 mobile animation
 let s2Index = 0;
 
 function animateS2() {
@@ -108,21 +114,61 @@ function animateS2() {
     s2Index = 0;
   }
   if (s2Index == 0) {
+    s2LinksAnchors[s2Links.length - 1].classList.remove("list-item-hover");
     s2Links[s2Links.length - 1].dispatchEvent(new Event("mouseleave"));
     s2Index++;
     s2Links[0].dispatchEvent(new Event("mouseenter"));
+    s2LinksAnchors[0].classList.add("list-item-hover");
   } else {
     s2Links[s2Index - 1].dispatchEvent(new Event("mouseleave"));
+    s2LinksAnchors[s2Index - 1].classList.remove("list-item-hover");
     s2Links[s2Index].dispatchEvent(new Event("mouseenter"));
+    s2LinksAnchors[s2Index].classList.add("list-item-hover");
     s2Index++;
   }
 }
 
 const startAnimationS2 = () => {
   animateS2();
-  setTimeout(startAnimationS2, 3000);
+  setTimeout(startAnimationS2, 1000);
 };
 
 if (window.innerWidth < 600) {
   startAnimationS2();
 }
+
+//scroll to function
+const main = document.getElementsByTagName("main")[0];
+const scrollToElement = (e, closeMenu) => {
+  if (closeMenu) toggle();
+  const element = document.getElementById(e);
+
+  element.addEventListener("click", (event) => {
+    event.preventDefault();
+  });
+  console.log(element.getBoundingClientRect().y);
+  main.scrollTop += element.getBoundingClientRect().y;
+};
+
+//s3 popup
+const s3 = document.getElementById("s3");
+const popUp = document.getElementById("popup");
+const createPopUp = (e) => {
+  const image = e.getAttribute("data-image");
+  const title = e.getElementsByTagName("h2")[0].innerText;
+  const info = e.getAttribute("data-info");
+  popUp.getElementsByTagName("img")[0].src = image;
+  popUp.getElementsByClassName("popUpTitle")[0].innerHTML = title;
+  popUp.getElementsByClassName("popUpText")[0].innerHTML = info;
+  popUp.style.display = "flex";
+  gsap.fromTo(
+    popUp,
+    { opacity: 0, scale: 0.2 },
+    { opacity: 1, scale: 1, duration: 0.3 }
+  );
+  s3.style.backgroundImage = `url(${image})`;
+};
+
+const closePopUp = () => {
+  gsap.to(popUp, { opacity: 0, scale: 0.1, display: "none", duration: 0.3 });
+};
